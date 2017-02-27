dc6b6785c72c967857fde05cae924988fd3acd24
Dominik Broj <dominik.broj@dataart.com>
2017-02-27 11:29:27 +0100

    style FilterTodos, show current filter

--- src/components/FilterTodos/FilterTodos.component.js
+++ src/components/FilterTodos/FilterTodos.component.js
@@ -1,22 +1,40 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import styles from './FilterTodos.stylesheet.css';
+import classNames from 'classnames';
 
-const FilterTodos = ({ filters, onFilterChosen }) => {
+class FilterTodos extends Component {
   
-  const handleFilterChosen = (filter) => {
-    onFilterChosen(filter);
+  constructor() {
+    super();
+    this.handleFilterChosen = this.handleFilterChosen.bind(this);
+    this.state = {
+      currentFilterIndex: 2
+    };
+  }
+  
+  handleFilterChosen = (filter, index) => {
+    this.props.onFilterChosen(filter);
+    this.setState({ currentFilterIndex: index });
   };
   
-  return (
-    <ul className={styles.FilterTodos}>
-      {filters.map((filter, index) => (
-        <button key={index} onClick={() => handleFilterChosen(filter)}>
-          {filter}
-        </button>
-      ))}
-    </ul>
-  )
-};
+  render() {
+    return (
+      <ul className={styles.FilterTodos}>
+        {this.props.filters.map((filter, index) => (
+          <button key={index}
+                  onClick={() => this.handleFilterChosen(filter, index)}
+                  className={classNames(
+                    styles.filterButton,
+                    { [styles.activeButton]: index===this.state.currentFilterIndex }
+                  )}
+                  >
+            {filter}
+          </button>
+        ))}
+      </ul>
+    )
+  };
+}
 
 FilterTodos.propTypes = {
   filters: PropTypes.arrayOf(PropTypes.string),
